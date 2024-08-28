22c9447c1ff53b0263305711a92787159228882c
Cristhian Vargas <cristhianvar09@gmail.com>
2024-08-28 18:52:00 -0500

    App deploy setup in GitHub

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Travel_Rate_Interface_App/',  // Añadir esta línea con el nombre del repositorio
   server: {
     port: 5500, // Cambia este valor al puerto que prefieras
 }
